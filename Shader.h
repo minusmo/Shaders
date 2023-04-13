58dc268bf185100f74d612799a4841b1487b9038
hojooneum <bldolphin96@gmail.com>
2023-04-13 23:23:48 +0900

    shader(vertex, fragment) implementation for practice

--- Shader.h
+++ Shader.h
@@ -5,6 +5,12 @@
 #ifndef SHADERS_SHADER_H
 #define SHADERS_SHADER_H
 
+#include "iostream"
+#include "string"
+#include "fstream"
+#include "sstream"
+#include "glad/include/glad/glad.h"
+
 class Shader {
     unsigned int ID;
 
@@ -14,24 +20,62 @@ class Shader {
         // Geometry shader is optional.
         // In this step, you should make sure the files are loaded successfully.
         // Unless, throw exception and you should catch it.
-
+            std::string vertexShaderCode;
+            std::string fragmentShaderCode;
+            std::ifstream vertexShaderFile;
+            std::ifstream fragmentShaderFile;
+            vertexShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit)
+            fragmentShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit)
+        try {
+            // Open each file.
+            vertexShaderFile.open(vertexShaderFileName);
+            fragmentShaderFile.open(fragmentShaderFileName);
+            // Read each file into string buffer.
+            std::stringstream vertexStream, fragmentStream;
+            vertexStream << vertexShaderFile.rdbuf();
+            fragmentStream << fragmentShaderFile.rdbuf();
+            // Once reading buffer is end, Close the files.
+            vertexShaderFile.close();
+            fragmentShaderFile.close();
+            // And now, assign code as string from string stream.
+            vertexShaderCode = vertexStream.str();
+            fragmentShaderCode = fragmentStream.str();
+        }
+        catch (std::ifstream::failure& e) {
+            std::cout << "ERROR::SHADER::FILE_NOT_SUCCESSFULLY_READ: " << e.what() << std::endl;
+        }
         // 2. Once you load the source files for vertex and fragment shaders successfully,
-        // you should save them into c_string.
-
+        // you should save them into c_string which is char buffer.
+        const char* vSCode = vertexShaderCode.c_str();
+        const char* fSCode = fragmentShaderCode.c_str();
         // For each shader, you do following steps.
         // a. Create shader using glCreateShader
+        unsigned int vShader, fShader;
+        vShader = glCreateShader();
+        fShader = glCreateShader();
         // b. Link the shader to shader source file with glShaderSource
+        glShaderSource(vShader,1,vertexShaderCode,NULL);
+        glShaderSource(fShader,1,fragmentShaderCode,NULL);
         // c. Compile the shader with glCompileShader
+        glCompileShader(vShader);
+        glCompileShader(fShader);
         // d. Check the compile errors for shader compilation.
-
+        checkCompileErrors(vShader, "VERTEX");
+        checkCompileErrors(fShader, "FRAGMENT");
         // 3. So you compiled each shader, you link the shaders to shader program.
         // You create shader program with glCreateProgram().
+        ID = glCreateProgram()
         // Then, attach each shader to the program with glAttachShader().
+        glAttachShader(ID, vShader);
+        glAttachShader(ID, fShader);
         // Finally, link the program with glLinkProgram().
+        glLinkProgram(ID);
         // Last but not least, check compile errors for the program link.
-
+        checkCompileErrors(ID, "PROGRAM");
         // 4. To Wrap the construction up, delete each shader no longer needed.
         // With glDeleteShader().
+        glDeleteShader(vShader);
+        glDeleteShader(fShader);
     }
     // For the ease of use, you can add few public functions.
 
